6c52296ea0bedf622f56b11a85c58eac90995c3f
rudasics <rudasics@cs.washington.edu>
2018-01-23 19:58:33 -0800

    Setup
    Setup All Unity Projects for all acts, with simple VR setup scene.

---
+++ Tyrant_VR_Act_3/Assets/scripts/CameraHeight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CameraHeight : MonoBehaviour {
+
+	//fields
+	public GameObject cameraContainer;
+	public float height;
+
+	// Use this for initialization
+	void Start () {
+		cameraContainer.transform.position = new Vector3 (0, height, 0);	
+	}
+}
